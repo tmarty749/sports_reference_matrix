a8a7328b4562806f122c949cbc2dd8105559ff1f
tmarty749 <tony.e.martin.ii@gmail.com>
2025-12-28 09:55:41 -0800

    Add files via upload
    added matrix script

---
+++ sports_reference.py
@@ -0,0 +1,177 @@
+import pandas as pd
+
+data= {
+
+'BRO': {
+
+'BSN': { 'W': 10, 'L': 12 },
+
+'CHC': { 'W': 15, 'L': 7 },
+
+'CIN': { 'W': 15, 'L': 7 },
+
+'NYG': { 'W': 14, 'L': 8 },
+
+'PHI': { 'W': 14, 'L': 8 },
+
+'PIT': { 'W': 15, 'L': 7 },
+
+'STL': { 'W': 11, 'L': 11 }
+
+},
+
+'BSN': {
+
+'BRO': { 'W': 12, 'L': 10 },
+
+'CHC': { 'W': 13, 'L': 9 },
+
+'CIN': { 'W': 13, 'L': 9 },
+
+'NYG': { 'W': 13, 'L': 9 },
+
+'PHI': { 'W': 14, 'L': 8 },
+
+'PIT': { 'W': 12, 'L': 10 },
+
+'STL': { 'W': 9, 'L': 13 }
+
+},
+
+'CHC': {
+
+'BRO': { 'W': 7, 'L': 15 },
+
+'BSN': { 'W': 9, 'L': 13 },
+
+'CIN': { 'W': 12, 'L': 10 },
+
+'NYG': { 'W': 7, 'L': 15 },
+
+'PHI': { 'W': 16, 'L': 6 },
+
+'PIT': { 'W': 8, 'L': 14 },
+
+'STL': { 'W': 10, 'L': 12 }
+
+},
+
+'CIN': {
+
+'BRO': { 'W': 7, 'L': 15 },
+
+'BSN': { 'W': 9, 'L': 13 },
+
+'CHC': { 'W': 10, 'L': 12 },
+
+'NYG': { 'W': 13, 'L': 9 },
+
+'PHI': { 'W': 13, 'L': 9 },
+
+'PIT': { 'W': 13, 'L': 9 },
+
+'STL': { 'W': 8, 'L': 14 }
+
+},
+
+'NYG': {
+
+'BRO': { 'W': 8, 'L': 14 },
+
+'BSN': { 'W': 9, 'L': 13 },
+
+'CHC': { 'W': 15, 'L': 7 },
+
+'CIN': { 'W': 9, 'L': 13 },
+
+'PHI': { 'W': 12, 'L': 10 },
+
+'PIT': { 'W': 15, 'L': 7 },
+
+'STL': { 'W': 13, 'L': 9 }
+
+},
+
+'PHI': {
+
+'BRO': { 'W': 8, 'L': 14 },
+
+'BSN': { 'W': 8, 'L': 14 },
+
+'CHC': { 'W': 6, 'L': 16 },
+
+'CIN': { 'W': 9, 'L': 13 },
+
+'NYG': { 'W': 10, 'L': 12 },
+
+'PIT': { 'W': 13, 'L': 9 },
+
+'STL': { 'W': 8, 'L': 14 }
+
+},
+
+'PIT': {
+
+'BRO': { 'W': 7, 'L': 15 },
+
+'BSN': { 'W': 10, 'L': 12 },
+
+'CHC': { 'W': 14, 'L': 8 },
+
+'CIN': { 'W': 9, 'L': 13 },
+
+'NYG': { 'W': 7, 'L': 15 },
+
+'PHI': { 'W': 9, 'L': 13 },
+
+'STL': { 'W': 6, 'L': 16 }
+
+},
+
+'STL': {
+
+'BRO': { 'W': 11, 'L': 11 },
+
+'BSN': { 'W': 13, 'L': 9 },
+
+'CHC': { 'W': 12, 'L': 10 },
+
+'CIN': { 'W': 14, 'L': 8 },
+
+'NYG': { 'W': 9, 'L': 13 },
+
+'PHI': { 'W': 14, 'L': 8 },
+
+'PIT': { 'W': 16, 'L': 6 }
+
+}
+
+}
+
+
+# Flatten nested dictionary into a list (to make it compatable w/ pandas)
+records = []
+for team, opponents in data.items():
+    for opponent, stats in opponents.items():
+        records.append({
+            'Team' : team,
+            'Opponent' : opponent,
+            'Wins' : stats['W']
+        })
+df = pd.DataFrame(records)
+
+# Index = Rows, Columns = Columns, Values = The Win count
+matrix = df.pivot(index='Team', columns='Opponent', values='Wins')
+
+#convert to integer (usung a formatter function)/Fill (NaN)
+def format_cell(val):
+    if pd.isna(val):
+        return "--"
+    return str(int(val))
+results_table = matrix.applymap(format_cell)
+
+# clean terminal output
+print("\nHead-to-Head Records Matrix:\n")
+print(results_table.to_string(index_names=False, justify='center', col_space=5))
+print("-" * 50)
+print(f"{'Tm':<5}" + "  ".join(results_table.columns))
